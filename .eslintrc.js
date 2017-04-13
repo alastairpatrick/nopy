96cc571bf0d2c8a64d3369dde40e00c5979319af
Al Patrick <Al Patrick>
2017-04-12 23:03:04 -0700

    Fix line endings

--- .eslintrc.js
+++ .eslintrc.js
@@ -98,7 +98,7 @@ module.exports = {
         "line-comment-position": "error",
         "linebreak-style": [
             "error",
-            "windows"
+            "unix"
         ],
         "lines-around-comment": "error",
         "lines-around-directive": "error",
